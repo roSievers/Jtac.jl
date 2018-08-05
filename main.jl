a9d4a9992514465286f1e4c87fde95b4be150499
Rolf Kreibaum <rolf.sievers@posteo.de>
2018-08-05 22:11:28 +0200

    place profiling code in a function

--- main.jl
+++ main.jl
@@ -22,9 +22,26 @@ function watch_ai_game(power = 1000)
     end
 end
 
-@time ai_turn!(new_game(), 1000)
-game = new_game()
-@profile ai_turn!(game)
-println("loading profile view")
-using ProfileView
-ProfileView.view()
+function profile()
+    @time ai_turn!(new_game(), 1000)
+    game = new_game()
+    @profile ai_turn!(game)
+    println("loading profile view")
+    using ProfileView
+    ProfileView.view()
+end
+
+function mctree_vs_random(power = 1000)
+    game = new_game()
+    while !game_result(game)[1]
+        if game.current_player == 1
+            ai_turn!(game, power)
+        else
+            random_turn!(game)
+        end
+        # print_board(game)
+        # println(" ")
+        # println(" ")
+    end
+    game_result(game)[2]
+end
