9368d5edf104ba86a53289d84d5422c999fe8ee1
Thomas Staudt <tscode@posteo.net>
2018-10-16 20:32:10 +0200

    some refactoring in game.jl

--- src/game.jl
+++ src/game.jl
@@ -1,5 +1,9 @@
+# Jtacs: game.jl
 
-# A Jtacs Game
+# A Jtacs Game with two competing players. The game can only end by victory of
+# Player 1 or 2, or by draw. A realization of this abstract type is meant to
+# describe the state of the game, which can be modified by actions (represented
+# by action indices).
 abstract type Game end
 
 # Status of the game
@@ -9,7 +13,12 @@ abstract type Game end
 #const Status = Union{Nothing, Int}
 const Status = Int
 
-# We are not sure about the concrete implementation of Status yet,
+# The actions on a Jtacs game are represented by canonical indices
+# from 1 to policy_length(game)
+const ActionIndex = Int
+
+
+# We have not decided about the concrete implementation of Status yet,
 # so we wrap it in a Status constructor that has the following properties:
 #   - is_over(Status()) must return false
 #   - is_over(Status(value)) must return true for value = 1,0,-1
@@ -17,42 +26,28 @@ const Status = Int
 Status(value :: Int) = value
 Status() = 42
 
-is_over(s :: Status) = s != 42
+is_over(s :: Status) = (s != 42)
 
 with_default(s :: Status, default :: Int) :: Int = is_over(s) ? s : default
-
-# The actions of a Jtacs Game are represented by canonical indices
-# from 1 to policy_length(game)
-const ActionIndex = Int
 
 
 # When using a game with broadcasting functionality, 
 # do not try to iterate over it
 Broadcast.broadcastable(game :: Game) = Ref(game)
- 
-Base.copy(:: Game) = error("unimplemented") 
-
-function legal_actions(:: Game) :: Vector{ActionIndex}
-  error("unimplemented")
-end
-
-function apply_action!(:: Game, :: ActionIndex) :: Nothing
-  error("unimplemented")
-end
-
-function policy_length(:: Type{Game}) :: Int
-  error("unimplemented")
-end
-
-function policy_length(game :: G) :: Int where G <: Game
-  policy_length(G)
-end
-
-# Return the game status
-status(game :: Game) :: Status = error("unimplemented")
 
 # Whether the game has finished yet
 is_over(game :: Game) = is_over(status(game))
+ 
+
+#
+# Interface functions that have to be implemented by concrete games
+#
+
+Base.copy(:: Game) :: Game = error("unimplemented") 
+status(game :: Game) :: Status = error("unimplemented")
+
+legal_actions(:: Game) :: Vector{ActionIndex} = error("unimplemented")
+apply_action!(:: Game, :: ActionIndex) :: Nothing = error("unimplemented")
 
 # Data representation of the game as layered 2d image
 representation(:: Game) :: Array{Float32, 3} = error("unimplemented")
@@ -60,7 +55,18 @@ representation(:: Game) :: Array{Float32, 3} = error("unimplemented")
 # Size of the data representation of the game
 Base.size(:: Game) :: Tuple{Int, Int, Int} = error("unimplemented")
 
+# Length of the policy vector
+policy_length(:: Type{Game}) :: Int = error("unimplemented")
+
+
+#
 # Convenience functions 
+#
+
+function policy_length(game :: G) :: Int where G <: Game
+  policy_length(G)
+end
+
 random_action(game :: Game) = rand(legal_actions(game))
 random_turn!(game :: Game)  = apply_action!(game, random_action(game))
 
@@ -74,4 +80,11 @@ end
 function random_playout(game :: Game) :: Game
   random_playout!(copy(game))
 end
+
+
+#
+# Optional interface functions
+#
+
+draw(:: Game) :: Nothing = error("drawing not available for games of type $(typeof(game))")
 
