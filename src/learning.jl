076455cabfd777ba6167b5df047af368eeb5249b
Rolf Kreibaum <rolf.sievers@posteo.de>
2018-11-04 18:01:30 +0100

    Fix bugs in learning.jl

--- src/learning.jl
+++ src/learning.jl
@@ -1,41 +1,49 @@
 # This module collects functions which help us train models.
 
 # Stores a replay of a selfplay which we may use to train the model
-mutable struct SelfplayRecord{G <: Game}
+mutable struct SelfplayReplay{G <: Game}
   game_states :: Vector{G}
   # A list of normalized node.visit_counter
-  posterior_distributions :: Vector{Vector{Float64}}
+  posterior_distributions :: Vector{Vector{Float32}}
   # Possible values are -1, 0, 1.
-  game_result :: Float64
+  game_result :: Float32
+end
+
+function SelfplayReplay{G}() where G <: Game
+  SelfplayReplay(Vector{G}(), Vector{Vector{Float32}}(), Float32(0))
 end
 
 # Calculates the loss function for a single game moment from a selfplay.
-function loss(game_state :: Game, posterior :: Vector{Float64}, result :: Float64, model :: Model) :: Float64
-  value, policy = apply(model, game)
+function loss(game_state :: Game, posterior :: Vector{Float32}, result :: Float32, model :: Model) :: Float32
+  value, policy = apply(model, game_state)
   value_loss = (value - result * game_state.current_player)^2
-  cross_entropy_loss = - posterior * log.(policy)
+  cross_entropy_loss = - sum(posterior .* log.(policy))
 
   value_loss + cross_entropy_loss
 end
 
 # Calculates the loss function for a whole selfplay
 function loss(replay :: SelfplayReplay, model :: Model)
-  sum :: Float64 = 0
-  for i = 1:size(replay.game_states)
+  sum :: Float32 = 0
+  for i = 1:length(replay.game_states)
     sum += loss(replay.game_states[i], replay.posterior_distributions[i], replay.game_result, model)
   end
   sum
 end
 
 # Executes a selfplay and returns the Replay
-function record_selfplay(game; power = 100, model = RolloutModel(game)) :: SelfplayReplay
+function record_selfplay(game :: G; power = 100, model = RolloutModel(game)) :: SelfplayReplay where G <: Game
   game = copy(game)
-  replay = SelfplayReplay()
+  replay = SelfplayReplay{G}()
   while !is_over(game)
     push!(replay.game_states, copy(game))
+    actions = legal_actions(game)
     node = mctree_turn!(game, power = power, model = model)
+
     posterior_distribution = node.visit_counter / sum(node.visit_counter)
-    push!(replay.posterior_distributions, posterior_distribution)
+    improved_policy = zeros(81)
+    improved_policy[actions] = posterior_distribution
+    push!(replay.posterior_distributions, improved_policy)
   end
   replay.game_result = status(game)
   replay
