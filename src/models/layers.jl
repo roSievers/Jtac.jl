31245fb910e021246d24cc33ad890c88f4fff065
Thomas Staudt <tscode@posteo.net>
2018-11-19 01:17:31 +0100

    can now push layers on gpu, still need models

--- src/models/layers.jl
+++ src/models/layers.jl
@@ -3,57 +3,96 @@
 using AutoGrad
 using Knet
 
-# Auxiliary activation functions
+# Auxiliary functions
 
+# Identity. Need to give the primitive due to current AutoGrad limitations
 id(x) = x
-# need to give the primitive due to current AutoGrad limitations
 @primitive id(x),dy (dy)
 
+# Check if something is a AutoGrad param or not
+is_param(array) = typeof(array) <: Param
 
-# Dense layer
+# Layers
 
-struct Dense
+abstract type Layer{GPU} end
+
+# Conversion of layers from and to cpu
+to_cpu(l :: Layer{false}) :: Layer{false} = l
+to_gpu(l :: Layer{true}) :: Layer{true} = l
+
+to_cpu(l :: Layer{true}) :: Layer{false} = swap(l)
+to_gpu(l :: Layer{false}) :: Layer{true} = swap(l)
+
+swap(l :: Layer) = error("Not implemented")
+
+# Check if a layer lives on the gpu
+on_gpu(:: Layer{GPU}) :: Bool = GPU
+
+# Copy a layer
+Base.copy(l :: Layer) :: Layer = error("Not implemented")
+
+# Convert GPU :: Bool to the representing array type
+atype(gpu :: Bool) = gpu ? KnetArray{Float32} : Array{Float32}
+
+
+# Dense layers
+
+struct Dense{GPU} <: Layer{GPU}
   w  # Weight matrix 
   b  # Bias vector
   f  # Activation function
 end
 
-function Dense(i :: Int, o :: Int, f = relu, bias = true)
-  if bias
-    Dense(param(o,i), param0(o), f)
-  else
-    Dense(param(o,i), zeros(Float32, o), f)
-  end
+function Dense(i :: Int, o :: Int, f = relu; bias = true, gpu = false)
+  at = atype(gpu)
+  w = param(o,i, atype = at)
+  b = bias ? param0(o, atype = at) : convert(at, zeros(Float32, o))
+  Dense{gpu}(w, b, f)
 end
 
 (d :: Dense)(x) = d.f.(d.w * mat(x) + d.b)
 
+function swap(d :: Dense{GPU}) where {GPU}
+  at = atype(!GPU)
+  w = convert(at, value(d.w))
+  b = convert(at, value(d.b))
+  Dense{!GPU}(Param(w), is_param(d.b) ? Param(b) : b, d.f)
+end
+
 
 # Convolutional layer
 
-struct Conv
+struct Conv{GPU} <: Layer{GPU}
   w  # Convolutional kernel
   b  # Bias vector
   f  # Activation function
 end
 
-function Conv(ci :: Int, co :: Int, f = relu; kernelsize :: Int = 3, bias = true)
-  if bias
-    Conv(param(kernelsize, kernelsize, ci, co), param0(1,1,co,1), f)
-  else
-    Conv(param(kernelsize, kernelsize, ci, co), zeros(Float32,1,1,co,1), f)
-  end
+function Conv(ci :: Int, co :: Int, f = relu; 
+              kernelsize :: Int = 3, bias = true, gpu = false)
+  at = atype(gpu)
+  w = param(kernelsize, kernelsize, ci, co, atype = at)
+  b = bias ? param0(1, 1, co, 1, atype = at) : convert(at, zeros(Float32, 1, 1, co, 1))
+  Conv{gpu}(w, b, f)
 end
 
 (c :: Conv)(x) = c.f.(conv4(c.w, x) .+ c.b)
 
+function swap(c :: Conv{GPU}) where {GPU}
+  at = atype(!GPU)
+  w = convert(at, value(c.w))
+  b = convert(at, value(c.b))
+  Conv{!GPU}(Param(w), is_param(c.b) ? Param(b) : b, c.f)
+end
+
+
 # Chaining of layers
 
-struct Chain
+struct Chain{GPU} <: Layer{GPU}
   layers
 end
 
-Chain(layers...) = Chain(layers)
+Chain(layers :: Layer{GPU}...) where {GPU} = Chain{GPU}(layers)
 
 function (c :: Chain)(x)
   for l in c.layers
@@ -61,3 +100,7 @@ function (c :: Chain)(x)
   end
   x
 end
+
+swap(c :: Chain{GPU}) where {GPU} = Chain{!GPU}(swap.(c.layers)...)
+
+
