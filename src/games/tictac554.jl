da5ccc58d01a037182aa215a392d6a7640ed6cc3
Rolf Kreibaum <rolf.sievers@posteo.de>
2019-06-10 12:40:31 +0200

    use common util function

--- src/games/tictac554.jl
+++ src/games/tictac554.jl
@@ -129,22 +129,6 @@ function representation(game :: TicTac554) :: Array{Float32, 3}
   reshape(game.current_player .* game.board, (5, 5, 1))
 end
 
-hmirror(matrix) = matrix[end:-1:1, :]
-vmirror(matrix) = matrix[:, end:-1:1]
-
-function apply_dihedral_group(matrix)
-  [
-    matrix |> copy,
-    matrix |> hmirror,
-    matrix |> transpose,
-    matrix |> vmirror,
-    matrix |> transpose |> hmirror,
-    matrix |> hmirror |> transpose,
-    matrix |> vmirror |> hmirror,
-    matrix |> vmirror |> hmirror |> vmirror
-  ]
-end
-
 function augment(game :: TicTac554, label :: Vector{Float32})
   boards = apply_dihedral_group(reshape(game.board, (5,5))) 
   games = [ TicTac554(reshape(b, (25,)), game.current_player, game.status) for b in boards ]
