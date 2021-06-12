13b482d88a7a0199880c0d7c7c98addb39c4c245
Thomas Staudt <tscode@posteo.net>
2021-06-12 19:05:27 +0200

    partial adaption of server to module structure of Jtac

--- JtacServer/src/log.jl
+++ JtacServer/src/log.jl
@@ -1,84 +1,35 @@
 
-module Stl
+const prefix = Styled.comment("jtac │")
+const swarn  = Styled.warn("warning:")
+const serror = Styled.error("ERROR:")
+const sdebug = Styled.debug("DEBUG:")
 
-  import ..Msg
+info(io :: IO, str)  = println(io, "$prefix $str")
+warn(io :: IO, str)  = println(io, "$prefix $swarn $str")
+error(io :: IO, str) = println(io, "$prefix $serror $str")
 
-  struct Style
-    text :: String
-    bold :: Bool
-    color :: Union{Int, Symbol}
-  end
+function debug(io :: IO, str)
+  if DEBUG[] println(io, "$prefix $sdebug $str") end
+end
 
-  function Style(txt :: String; bold = false, color = :normal)
-    Style(txt, bold, color)
-  end
+info(args :: String ...)  = info(stdout, args...)
+warn(args :: String ...)  = warn(stdout, args...)
+error(args :: String ...) = error(stdout, args...)
+debug(args :: String ...) = debug(stdout, args...)
 
-  # Some automatic style conversions
-  Style(x :: Real; kw...) = Style(Base.string(x); kw...)
+function info(c :: RemoteChannel{Channel{String}}, str)
+  put!(c, str)
+end
 
-  Style(req :: Msg.ToPlay.DataReq; kw...) = Style("D$(req.reqid)"; kw...)
-  Style(req :: Msg.ToPlay.ContestReq; kw...) = Style("C$(req.reqid)"; kw...)
+function warn(c :: RemoteChannel{Channel{String}}, str)
+  put!(c, "$swarn $str")
+end
 
-  Style(x :: Msg.FromPlay.Data; kw...) = Style("d$(x.id)"; kw...)
-  Style(x :: Msg.FromPlay.Contest; kw...) = Style("c$(x.id)"; kw...)
+function error(c :: RemoteChannel{Channel{String}}, str)
+  put!(c, "$serror $str")
+end
 
-
-  function Base.show(io :: IO, s :: Style)
-    ioc = IOContext(io, :color => true)
-    printstyled(ioc, s.text, bold = s.bold, color = s.color)
-  end
-
-  name(str)    = Style(str; color = :green)
-  keyword(str) = Style(str; color = :cyan)
-  quant(str)   = Style(str; color = :yellow)
-  faulty(str)  = Style(str; color = :red)
-  warn(str)    = Style(str; color = :red)
-  error(str)   = Style(str; color = :red, bold = true)
-  string(str)  = Style(str; color = :yellow)
-  comment(str) = Style(str; color = 245)
-  debug(str)   = Style(str; color = :blue)
-
-end # module Stl
-
-
-module Log
-
-  import ..DEBUG
-  import ..Stl
-  import Distributed: RemoteChannel
-
-  prefix = Stl.comment("jtac │")
-  swarn  = Stl.warn("warning:")
-  serror = Stl.error("ERROR:")
-  sdebug = Stl.debug("DEBUG:")
-
-  info(io :: IO, str)  = println(io, "$prefix $str")
-  warn(io :: IO, str)  = println(io, "$prefix $swarn $str")
-  error(io :: IO, str) = println(io, "$prefix $serror $str")
-  function debug(io :: IO, str)
-    if DEBUG[] println(io, "$prefix $sdebug $str") end
-  end
-
-  info(args :: String ...)  = info(stdout, args...)
-  warn(args :: String ...)  = warn(stdout, args...)
-  error(args :: String ...) = error(stdout, args...)
-  debug(args :: String ...) = debug(stdout, args...)
-
-  function info(c :: RemoteChannel{Channel{String}}, str)
-    put!(c, str)
-  end
-
-  function warn(c :: RemoteChannel{Channel{String}}, str)
-    put!(c, "$swarn $str")
-  end
-
-  function error(c :: RemoteChannel{Channel{String}}, str)
-    put!(c, "$serror $str")
-  end
-
-  function debug(c :: RemoteChannel{Channel{String}}, str)
-    if DEBUG[] put!(c, "$sdebug $str") end
-  end
-
-end # module Log
+function debug(c :: RemoteChannel{Channel{String}}, str)
+  if DEBUG[] put!(c, "$sdebug $str") end
+end
 
